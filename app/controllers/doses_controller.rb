9c8ff1e342f4fae8ec55e97a298fbdb5db2a014d
francisco gaivao <francisco.abecassis.gaivao@gmail.com>
2018-02-22 15:13:12 +0000

    cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find (params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    cocktail = Cocktail.find (params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail
+    else
+      render :new
+  end
+  end
+  def destroy
+    Dose.find(params[:id]).destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:descriprion, :ingredient_id, :cocktail_id)
+  end
+end
